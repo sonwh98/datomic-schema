52651fcac1a7ff3a1de40709048d9c30acee4e95
sto <son.c.to@gmail.com>
2019-07-21 22:04:27 -0400

    datomic-free

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [stigmergy/tily "0.1.7-SNAPSHOT"]])
+                 [stigmergy/tily "0.1.7-SNAPSHOT"]]
+  
+  :source-paths ["src/cljc"]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5697"]]}})
